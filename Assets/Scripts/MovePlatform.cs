1189d8845f7646d653a8afb46e804de46fbdfa30
Chase Maslow <cmaslow@uoregon.edu>
2024-04-28 12:24:43 -0700

    Changed movement method to rb.MovePosition

--- Assets/Scripts/MovePlatform.cs
+++ Assets/Scripts/MovePlatform.cs
@@ -7,32 +7,39 @@ public class MovePlatform : MonoBehaviour
     // Start is called before the first frame update
     public float Offset = 0;
     public float speed = 0;
+
     private float leftLimit;
     private float rightLimit;
     private Rigidbody rb;
     private bool changed;
+	private float move = 0.0f;
 
     void Start()
     {
         leftLimit = transform.position.x - Offset;
         rightLimit = transform.position.x + Offset;
         rb = GetComponent<Rigidbody>();
-        rb.velocity = new Vector3(speed, 0, 0);
+        //rb.velocity = new Vector3(speed, 0, 0);
+		move = speed;
         changed = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+		//transform.Translate(Vector3.right * move * Time.deltaTime);
+		rb.MovePosition(transform.position + Vector3.right*move*Time.deltaTime);
+
         if (transform.position.x > rightLimit && changed)
         {
-            rb.velocity = new Vector3(rb.velocity.x * -1, rb.velocity.y, rb.velocity.z);
+            //rb.velocity = new Vector3(rb.velocity.x * -1, rb.velocity.y, rb.velocity.z);
+			move = -move;
             changed = !changed;
         }
         
         if (transform.position.x < leftLimit && !changed)
         {
-            rb.velocity = new Vector3(rb.velocity.x * -1, rb.velocity.y, rb.velocity.z);
+            //rb.velocity = new Vector3(rb.velocity.x * -1, rb.velocity.y, rb.velocity.z);
+			move = -move;
             changed = !changed;
         }
 
